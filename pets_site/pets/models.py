86b6be6afca23b977a3c35d745d1df8fbb658266
chelseacastelli <chelsea.castelli@students.makeschool.com>
2020-05-19 12:45:32 -0400

    [UPDATE] Pet and Appointment models

---
+++ pets_site/pets/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
+
+# Create your models here.
+class Pet(models.Model):
+    pet_name = models.CharField(max_length=200)
+    species = models.CharField(max_length=200)
+    breed = models.CharField(max_length=200)
+    weight_in_pounds = models.DecimalField(max_digits=5, decimal_places=2)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Appointment(models.Model):
+    date_of_appointment = models.DateField(default=datetime.now)
+    duration_minutes = models.IntegerField()
+    special_instructions = models.CharField(max_length=400)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
