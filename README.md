a2200f61973fdf32251b7ed376b7593baf9b06d1
chelseacastelli <chelsea.castelli@students.makeschool.com>
2020-05-20 11:01:56 -0400

    [UPDATE] README and tests

---
+++ README.md
@@ -0,0 +1,11 @@
+##Pet House Sitting Site
+
+###Create a pet sitting service app. Your customers will be able to enter information for their pets (e.g. name, breed, and special instructions). Then, they will be able to book pet sitting appointments and will be assigned a pet sitter.
+
+###This priject reinforces the following concepts:
+- Views, URLs, Templates, & Models
+- Authentication & Authorization
+- Forms
+- Automated Testing
+
+You can access the admin site with username `chelseaannecastelli` and password `djangopony`.
--- pets_site/pets/tests.py
+++ pets_site/pets/tests.py
@@ -9,25 +9,82 @@ from pets.models import Pet, Appointment
 # Create your tests here.
 class PetTests(TestCase):
     def test_list_page(self):
-        # Instance of user to test the pages
+
         user = User.objects.create()
 
-        # Create a test pet
         pet = Pet.objects.create(pet_name="Nicky",
                                  species="Dog",
                                  breed='Pomchi',
                                  weight_in_pounds=9,
                                  owner=user)
-        pet.save()  # save our pet
+        pet.save()
 
-        # Making a GET request to check if the pet is in the results (list page)
         res = self.client.get(f'/pets/{pet.id}/')
-        # Very a 200 response
+
         self.assertEqual(res.status_code, 200)
-        # Check if the page contains Captain America (our title)
         self.assertContains(res, 'Nicky')
-        # Get pet object to check if the user's pet exists
+
         pet_object = Pet.objects.get(owner=user)
         self.assertEqual(pet_object.owner, user)
 
+        def test_detail_page(self):
+            user = User.objects.create_user(username='admin', password='djangopony')
+            self.client.login(username='admin', password='djangopony')
 
+            pet = Pet.objects.create(pet_name='Bruce',
+                                     species='Cat',
+                                     breed='British Shorthair',
+                                     weight_in_pounds=9.8,
+                                     owner=user)
+            pet.save()
+
+            appointment = Appointment.objects.create(date_of_appointment=timezone.now(),
+                                                     duration_minutes=30,
+                                                     special_instructions='She isn\'t playful',
+                                                     pet=pet)
+            appointment.save()
+            res = self.client.get(f'/pets/{pet.id}/')
+            self.assertEqual(res.status_code, 200)
+
+            pet_object = Pet.objects.get(pet_name='Bruce')
+            self.assertEqual(pet_object.pet_name, 'Bruce')
+
+        def test_pet_create_page(self):
+            user = User.objects.create()
+            post_data = {
+                'pet_name': 'Spot',
+                'species': 'Dog',
+                'breed': 'Hound',
+                'weight_in_pounds': 68,
+                'owner': user.id
+            }
+
+            res = self.client.post('/pet/create/', data=post_data)
+            self.assertEqual(res.status_code, 302)
+
+            pet_object = Pet.objects.get(pet_name='Spot')
+            self.assertEqual(pet_object.pet_name, 'Spot')
+
+        def test_create_appointment_page(self):
+            user = User.objects.create()
+
+            pet = Pet.objects.create(pet_name='Holly',
+                                     species='Dog',
+                                     breed='Mix',
+                                     weight_in_pounds=50,
+                                     owner=user)
+            pet.save()
+
+            post_data = {
+                'date_of_appointment': '2020-2-2',
+                'duration_minutes': 90,
+                'special_instructions': 'Scoop and a half of her food, twice',
+                'pet': pet
+            }
+
+            res = self.client.post('/appointment/create/', data=post_data)
+            self.assertEqual(res.status_code, 200)
+
+            app_object = Appointment.objects.get(date_of_appointment='2020-2-2')
+            self.assertEqual(app_object.date_of_appointment,
+                             datetime.date(2020, 2, 2))
